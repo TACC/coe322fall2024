0b53ffb705257ec42c4d671a39c56eaf5aacc52a
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-10-22 16:26:40 -0500

    solution so far

--- skeletons/catch2/require.cpp
+++ skeletons/catch2/require.cpp
@@ -23,36 +23,45 @@ TEST_CASE( "needs to be 5" ) {
 }
 //codesnippet end
 
-//codesnippet catchtestno
 float fiveish() { return 5.00001; }
 TEST_CASE( "not six" ) {
   // this will fail
-  REQUIRE( fiveish()==5 );
+  REQUIRE( fiveish()!=5 );
   // this will succeed
   REQUIRE( fiveish()==Catch::Approx(5) );
 }
-//codesnippet end
 
-//codesnippet catchevenfun
+bool is_prime( int n ) {
+  if (n<=0) throw("negative number");
+  if (n==1) return false;
+  //  if (n==2) return true;
+  for ( int factor=2; factor<n; ++factor )
+    if (n%factor==0) return false;
+  return true;
+}
+
+TEST_CASE( "primality testing" ) {
+  REQUIRE_THROWS( is_prime(-1) );
+  REQUIRE_NOTHROW( is_prime(1) );
+  REQUIRE( not is_prime(1) );
+  REQUIRE( is_prime(2) );
+}
+
+#if 0
+
 void even( int e ) {
     if (e%2==1) throw(1);
     cout << "Even number: "
          << e << '\n';
 }
-//codesnippet end
 
-//codesnippet catchtesteven
 TEST_CASE( "even fun" ) {
     REQUIRE_NOTHROW( even(2) );
     REQUIRE_THROWS( even(3) );
 }
-//codesnippet end
 
-//codesnippet catchtestgen
 TEST_CASE( "even set" ) {
     int e = GENERATE( 2,4,6,8 );
     REQUIRE_NOTHROW( even(e) );
 }
-//codesnippet end
-
-
+#endif
